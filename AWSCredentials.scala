3644d2de40693b1a7d27c3d9b0954e9b0cc74f34
Myles Daniel Baker <mylesdanielbaker@gmail.com>
2017-06-06 11:01:21 -0400

    Added createSession() function, which writes session credentials to disk on cluster

--- AWSCredentials.scala
+++ AWSCredentials.scala
@@ -3,6 +3,17 @@
  * WARNING: This class for storing credentials is insecure
  *          and should be used for demonstration purposes only
  */
+
+import java.io.File
+import java.io.PrintWriter
+
+def writeToFile(p: String, s: String): Unit = {
+    val f = new File(p)
+    f.getParentFile().mkdirs();
+    val pw = new PrintWriter(f)
+    try pw.write(s) finally pw.close()
+}
+
 class AWSCredentials {
   private val _access = ""
   private val _secret = ""
@@ -16,4 +27,24 @@ class AWSCredentials {
   
   def endpoint_(value: String): Unit = _endpoint = value
   def region_(value: String): Unit = _region = value
+  
+  def createSession(): Unit = {
+    import java.io.File
+    import java.io.PrintWriter
+    
+    val p = """
+[default]
+aws_access_key_id = """+this._access+"""
+aws_secret_access_key = """+this._secret+"""
+region = """+this.region
+    
+    val r = """
+[default]
+aws_access_key_id = """+this._access+"""
+aws_secret_access_key = """+this._secret
+    
+    writeToFile("/root/.aws/config", p)
+    writeToFile("/root/.aws/credentials", r)
+  }
+  
 }
